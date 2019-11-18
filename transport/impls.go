2dfbd58a229629925b4e7fdbf3c48b02ed5ea488
William Katsak <bill@taekion.com>
2019-11-17 22:26:52 -0500

    transport: add support for new zmq transport
    Signed-off-by: William Katsak <bill@taekion.com>

--- transport/impls.go
+++ transport/impls.go
@@ -3,6 +3,7 @@ package transport
 import (
 	"fmt"
 	"github.com/taekion-org/sawtooth-client-sdk-go/transport/rest"
+	"github.com/taekion-org/sawtooth-client-sdk-go/transport/zmq"
 	"net/url"
 )
 
@@ -11,12 +12,16 @@ type SawtoothClientTransportType int
 
 // TRANSPORT_REST represents the REST API transport implementation.
 const TRANSPORT_REST SawtoothClientTransportType = 1
+// TRANSPORT_ZMQ represents the ZMQ transport implementation.
+const TRANSPORT_ZMQ SawtoothClientTransportType = 2
 
 // NewSawtoothClientTransport instantiates and returns a new SawtoothClientTransport of the specified type.
 func NewSawtoothClientTransport(transportType SawtoothClientTransportType, url *url.URL) (SawtoothClientTransport, error) {
 	switch transportType {
 	case TRANSPORT_REST:
 		return rest.NewSawtoothClientTransportRest(url)
+	case TRANSPORT_ZMQ:
+		return zmq.NewSawtoothClientTransportZmq(url)
 	default:
 		return nil, fmt.Errorf("Unknown transport type")
 	}
