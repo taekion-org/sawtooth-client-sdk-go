890c15a9b267c3681b35ed4fee250d4246a87b11
William Katsak <bill@taekion.com>
2020-01-10 19:19:35 -0500

    transport: use strings for transport type consts
    Use strings instead of ints for transport type constants.
    
    Signed-off-by: William Katsak <bill@taekion.com>

--- transport/impls.go
+++ transport/impls.go
@@ -8,12 +8,12 @@ import (
 )
 
 // SawtoothClientTransportType represents an individual transport implementation.
-type SawtoothClientTransportType int
+type SawtoothClientTransportType string
 
 // TRANSPORT_REST represents the REST API transport implementation.
-const TRANSPORT_REST SawtoothClientTransportType = 1
+const TRANSPORT_REST SawtoothClientTransportType = "rest"
 // TRANSPORT_ZMQ represents the ZMQ transport implementation.
-const TRANSPORT_ZMQ SawtoothClientTransportType = 2
+const TRANSPORT_ZMQ SawtoothClientTransportType = "zmq"
 
 // NewSawtoothClientTransport instantiates and returns a new SawtoothClientTransport of the specified type.
 func NewSawtoothClientTransport(transportType SawtoothClientTransportType, url *url.URL) (SawtoothClientTransport, error) {
