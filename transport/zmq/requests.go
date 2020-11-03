19ad8efd3d120f49143e92008bb6ea669a60c582
William Katsak <bill@taekion.com>
2020-11-03 16:15:58 -0500

    transport/zmq: update handling of request errors
    Signed-off-by: William Katsak <bill@taekion.com>

--- transport/zmq/requests.go
+++ transport/zmq/requests.go
@@ -9,33 +9,36 @@ import (
 func (self *SawtoothClientTransportZmq) doZmqRequest(t validator_pb2.Message_MessageType, request proto.Message, response proto.Message) error {
 	requestMsg, err := proto.Marshal(request)
 	if err != nil {
-		return err
+		return errors.NewSawtoothClientTransportRequestError(err)
 	}
 
 	connection, err := self.getConnection()
 	if err != nil {
-		return err
+		return errors.NewSawtoothClientTransportRequestError(err)
 	}
 
 	corrId, err := connection.SendNewMsg(t, requestMsg)
 	if err != nil {
-		return err
+		return errors.NewSawtoothClientTransportRequestError(err)
 	}
 
 	_, responseMsg, err := connection.RecvMsgWithId(corrId)
 	err = proto.Unmarshal(responseMsg.GetContent(), response)
 	if err != nil {
-		return err
+		return errors.NewSawtoothClientTransportRequestError(err)
 	}
 
-	self.putConnection(connection)
+	err = self.putConnection(connection)
+	if err != nil {
+		return errors.NewSawtoothClientTransportRequestError(err)
+	}
 
 	errorCode := checkForError(response)
 	if errorCode != errors.NO_ERROR {
 		transportError := NewSawtoothClientTransportZmqError(t, request, response, errorCode)
 		err = &errors.SawtoothClientTransportError{
 			ErrorCode: transportError.ErrorCode,
-			TransportError: transportError,
+			ErrorObject: transportError,
 		}
 		return err
 	}
